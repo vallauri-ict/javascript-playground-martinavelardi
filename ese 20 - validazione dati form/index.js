133d06b607bb397e9c3550ab029a2d8db89c2bfe
martinavelardi <62564069+martinavelardi@users.noreply.github.com>
2020-04-15 16:35:03 +0200

    Update index.js

--- ese 20 - validazione dati form/index.js
+++ ese 20 - validazione dati form/index.js
@@ -5,47 +5,90 @@ let _nome;
 let _matricola;
 let _regione;
 let _email;
+let _tel;
 
 window.onload = function(){
     _cognome = document.getElementsByName("cognome")[0];
     _nome = document.getElementById("nome");
+    _matricola = document.getElementsByName("matricola")[0];
+    _regione = document.getElementsByName("regione")[0];
+    _email = document.getElementsByName("email")[0];
+    _tel = document.getElementsByName("tel")[0];
     // [TODO: mettere anche gli altri riferimenti]
 }
 
 function validaForm(){
+    //cognome
     if(_cognome.value == "")
     {
         _cognome.classList.add("red-border");
+        alert("Inserire il cognome!");
         return false;
     }
     else
     {
         _cognome.classList.remove("red-border");
     }
-    if (document.datiUtente.nome.value == ""){
+
+    //nome
+    if (_nome.value == ""){
+        _nome.classList.add("red-border");
         alert("Inserire il nome!");
         return false;
     }
-    if (document.datiUtente.matricola.value == "")
+    else
+    {
+        _nome.classList.remove("red-border");
+    }
+
+    //matricola
+    if (_matricola.value == "")
     {
         alert("Inserire matricola!");
+        _matricola.classList.add("red-border");
         return false;
     }
-    let v = parseInt(document.datiUtente.matricola.value);
+    else
+    {
+        _matricola.classList.remove("red-border");
+    }
+    let v = parseInt(_matricola.value);
     if (isNaN(v))
     {
         alert("La matricola deve essere un numero");
+        _matricola.classList.add("red-border");
+        _matricola.value="";
         return false;
     }
-    if (document.datiUtente.regione.value == "nessuna")
+    else
     {
+        _matricola.classList.remove("red-border");
+    }
+
+    //regione
+    if (_regione.value == "nessuna")
+    {
+        _regione.classList.add("red-select");
         alert("Selezionare una regione");
         return false;
     }
-    if (document.datiUtente.email.value == "" && document.datiUtente.tel.value == "")
+    else
     {
+        _regione.classList.remove("red-select");
+    }
+
+    //email e tel
+    if (_email.value == "" && _tel.value == "")
+    {
+        _email.classList.add("red-border");
+        _tel.classList.add("red-border");
         alert("Inserire l'email o il numero di telefono");
         return false;
+    }
+    else
+    {
+        _email.classList.remove("red-border");
+        _tel.classList.remove("red-border");
     }
     alert("Dati inseriti correttamente");
     return true;
